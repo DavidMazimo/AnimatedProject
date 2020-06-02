880b54a2770a52568c4202ca44b4060fe648ecfc
David mekonnen <davidsbusinessmain@gmail.com>
2020-06-02 21:26:46 +0300

    added todo List

--- script.js
+++ script.js
@@ -95,6 +95,61 @@ class Calculator {
   }
 }
 
+function addTodo(event) {
+  //prevent form from submitting
+  event.preventDefault();
+  //Todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //Check mark button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Check trash button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  //APPEND TO LIST
+  todoList.appendChild(todoDiv);
+  //clear todoInput value
+  todoInput.value = "";
+}
+
+function deleteCheck(e) {
+  const item = e.target;
+  console.log(item.classList[0]);
+  //DELETE
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //Check
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
+
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
 const numberButtons = document.querySelectorAll(".data-number");
 const operationButtons = document.querySelectorAll(".data-operation");
 const equalsButton = document.querySelector(".data-equals");
